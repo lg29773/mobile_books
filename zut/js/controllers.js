d20a092d7ee49c2bf4c647fa9bd51f0defa0764d
grzegorz <lg29773@zut.edu.pl>
2018-01-14 16:45:46 +0100

    Initial commit

---
+++ zut/js/controllers.js
@@ -0,0 +1,58 @@
+angular.module('app.controllers', [])
+     
+.controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('signupCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('loginCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('dostPneKsiKiCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('zamWieniaCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('kupioneCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('ksiazkaCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+ 
---
+++ zut/js/routes.js
@@ -0,0 +1,73 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+
+      .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+  .state('signup', {
+    url: '/page5',
+    templateUrl: 'templates/signup.html',
+    controller: 'signupCtrl'
+  })
+
+  .state('login', {
+    url: '/page6',
+    templateUrl: 'templates/login.html',
+    controller: 'loginCtrl'
+  })
+
+  .state('tabsController.dostPneKsiKi', {
+    url: '/page7',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/dostPneKsiKi.html',
+        controller: 'dostPneKsiKiCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.zamWienia', {
+    url: '/page8',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/zamWienia.html',
+        controller: 'zamWieniaCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.kupione', {
+    url: '/page9',
+    views: {
+      'tab3': {
+        templateUrl: 'templates/kupione.html',
+        controller: 'kupioneCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.ksiazka', {
+    url: '/page10',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/ksiazka.html',
+        controller: 'ksiazkaCtrl'
+      }
+    }
+  })
+
+$urlRouterProvider.otherwise('/page1/page7')
+
+
+});
